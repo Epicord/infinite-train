dd4fda9164db095cfa16b8bc3bb5241054b18597
Prappe <pw.td.gk@gmail.com>
2017-06-28 19:02:26 -0400

    rudimentary vertical camera movement
    When I see the camera it makes me HAVE SEX

--- Assets/Scripts/MainCameraMovement.cs
+++ Assets/Scripts/MainCameraMovement.cs
@@ -10,24 +10,36 @@ public class MainCameraMovement : MonoBehaviour
 
     private string cameraState = "followPlayer";
     private GameObject player;
-    private Vector3 defaultTransformOffset = new Vector3(0f, 4.25f, -4.5f); // behind and above the player
-    private Quaternion defaultRotationOffset = Quaternion.Euler(31f, 0f, 0f); // looking down towards the player
+    private Vector3 TransformOffset = new Vector3(0f, 4.25f, -4.5f); // camera moved behind and above the player
+    private Quaternion RotationOffset = Quaternion.Euler(31f, 0f, 0f); // camera rotated down towards the player
+    private Quaternion lookAngle = Quaternion.Euler(Vector3.zero); // camera rotation as controlled by the player
+    private float lookAngleOffset = 0f; // a useful number derived from lookAngle
 
     void Start ()
     {
         player = GameObject.Find("Player");
-        transform.position = player.transform.position + defaultTransformOffset;
-        transform.rotation = defaultRotationOffset;
-	}
+        transform.position = player.transform.position + player.transform.rotation * TransformOffset;
+        transform.rotation = player.transform.rotation * RotationOffset;
+    }
 	
 	void Update ()
     {
         if (cameraState == "followPlayer")
         {
-            // place the camera behind and above wherever the player is, accounting for the player's rotation
-            transform.position = player.transform.position + player.transform.rotation * defaultTransformOffset;
-            // face the camera the same direction the player is facing
-            transform.rotation = player.transform.rotation * defaultRotationOffset;
+            // get camera rotation from mouse movement
+            lookAngle *= Quaternion.Euler(-Input.GetAxisRaw("Mouse Y"), 0f, 0f);
+            // do some math to get a useful number: linear from 0 to 1, looking at the horizon = 0, looking downwards = 1
+            lookAngleOffset = ((lookAngle.eulerAngles.x + 121f) % 360 - 90) / 90;
+            // keep the camera behind the player, but get closer when looking downwards
+            TransformOffset.z = -5.75f + 5.75f * lookAngleOffset;
+            // if looking forwards/downwardsish, keep the camera at roughly the same vertical height, going a bit higher when looking downwards
+            if (lookAngleOffset >= 0) TransformOffset.y = 2.25f + 2 * lookAngleOffset;
+            // but get real low to the ground when looking upwards
+            else TransformOffset.y = 2.25f + 7 * lookAngleOffset;            
+            
+            // now actually move the camera where we want it to be
+            transform.position = player.transform.position + player.transform.rotation * TransformOffset;
+            transform.rotation = player.transform.rotation * RotationOffset * lookAngle;
         }
     }
 }
