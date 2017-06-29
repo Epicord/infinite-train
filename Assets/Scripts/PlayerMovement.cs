4a748bad8e171ad56365ba4557cc40dc7e823625
Prappe <pw.td.gk@gmail.com>
2017-06-28 21:31:27 -0400

    cleaned up PlayerMovement.cs
    bunch of notes we don't need and code we didn't use

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,30 +6,16 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // How fast the player moves. Higher = faster.
-    private const float movementSpeed = 7.2f;
-
+    private const float movementSpeed = 7.2f; // How fast the player moves. Higher = faster.
     public Vector3 move = Vector3.zero; // Stores the amount of movement on each frame.
-    private float slow = 1f;  // This number divides the overall movement by a value. This is used for diagonal movement.
-    private bool canJump = true; // Boolean to check if the player can jump or nah
-    private bool onGround = true; // Boolean to check if the player is on the ground
-
-    private float testpos = 0f;
-
-    public float jump; // Jump and gravity values can be modified in the unity editor in the Player object
-    public float gravity;
-
     // A Rigidbody is a component that helps to handle game physics. This is what we apply movement
     // to, as opposed to the player model itself, so that there is rudimentary collision support.
     private Rigidbody body;
     
-	// Use this for initialization
 	void Start ()
     {
         // Once everything loads, we can get the Rigidbody.
         body = GetComponent<Rigidbody>();
-        // A delicious testing variable for seeing if we can jump later.
-        testpos = body.position.y;
         // Lock the cursor so it can't escape our grasp. Camera controls.
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -41,95 +27,30 @@ public class PlayerMovement : MonoBehaviour
         Vector3 newPosition = transform.position;
         // Initalize a force vector.
         Vector3 applyForce = Vector3.zero;
-
-        // Let's test if we're on the ground.
-        if (body.position.y == testpos) // even after moving, we're still at the same y spot??
-        {
-            onGround = true; // that means we are on the ground
-            canJump = true; // and therefore can JUMP!!!
-        }
-        else // or maybe not.
-        {
-            onGround = false; // oh.
-            canJump = false; // that sucks.
-        }
-
-        // Testing position update, to see if we can jump later.
-        testpos = body.position.y;
-        slow = 1f;  // Reset variables
         move = Vector3.zero;
-        // Let's check the player's keyboard inputs.
+
         // If the player presses left...
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
             // Move left.
             // Time.deltaTime refers to the amount of time that has passed since the last frame.
             // Using this in object movement allows our game to run independently from framerate.
             move.x -= movementSpeed * Time.deltaTime;
-        }
-        // If the player presses right...
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
             // Move right.
             move.x += movementSpeed * Time.deltaTime;
-        }
-        // If the player presses up...
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            // Move up.
             move.z += movementSpeed * Time.deltaTime;
-        }
-        // If the player presses down...
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            // Move down.
             move.z -= movementSpeed * Time.deltaTime;
-        }
-        // If the player presses space...
-        if (Input.GetKey(KeyCode.Space) && canJump)
-        {
-            move.y = jump;
-            canJump = false;
-            onGround = false;
-        }
-        if (move.x != 0f && move.z != 0f) // Are we moving diagonally?
-        {
-            // If so, let's slow down the movement.
-            slow = 1.414f; // Approximately sqrt(2), because math
-        }
-        applyForce.y = move.y;           // update applyForce
-        newPosition += transform.rotation * move;  // set the movement, accounting for whichever direction the player is facing
-        body.MovePosition(newPosition); // and MOVE!
+
+        if (move.x != 0 && move.z != 0) move /= 1.414f; // make sure the player moves the same speed diagonally as they do orthogonally
+
+        applyForce.y = move.y; // update applyForce
+        newPosition += transform.rotation * move; // set the movement, accounting for whichever direction the player is facing
+        body.MovePosition(newPosition);
         body.AddForce(applyForce); // apply the force to the body
-        
-        if (body.position.y < 0.5 && move.y != jump) // player must be on baseplate and not trying to jump
-        {
-            Vector3 groundpos = body.position; // get the player position
-            groundpos.y = 0.5f; // make the y value on the ground
-            body.MovePosition(groundpos); // force him there
-            // i know there's a better way of doing this
-            // so for the love of god
-            // fix it
-        }
 
-        // Rotate the player with some sick mouselook controls, but counteract any accidental rotation from physics calculations.
-        transform.Rotate(new Vector3(-transform.rotation.eulerAngles.x, Input.GetAxisRaw("Mouse X"), -transform.rotation.eulerAngles.z));
-
-        // Gravity Falls Fanboys --v
-        if (onGround == false)
-        {
-            move.y -= gravity;
-            if (move.y < -100)
-            {
-                move.y = -100; // terminal velocity
-            }
-        }
-        else
-        {
-            move.y = 0; //stop falling jfc
-        }
-
-        // HACK: i am a professional hacker
-        // it's time to kill NASA
-	}
+        // horizontal mouselook rotation
+        transform.Rotate(new Vector3(0f, Input.GetAxisRaw("Mouse X"), 0f));
+    }
 }
